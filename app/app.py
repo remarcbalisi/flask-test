406460b569cc6f6c0912ea4ebaddf270c03dd078
remarcbalisi <remarcbalisi@gmail.com>
2016-02-20 23:31:14 +0800

    added git hub api

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__)
 
@@ -8,14 +9,28 @@ def hello():
 	return "Hello World!"
 
 
+# @app.route("/", methods=['GET', 'POST'])
+# def home():
+# 	if request.method == 'POST':
+# 		value_one = int(request.form.get('first'))
+# 		value_two = int(request.form.get('second'))
+# 		total = value_one + value_two
+# 		data = {'total':str(total)}
+# 		return jsonify(data)
+
+# 	return render_template('index.html')
+
+
 @app.route("/", methods=['GET', 'POST'])
 def home():
 	if request.method == 'POST':
-		value_one = int(request.form.get('first'))
-		value_two = int(request.form.get('second'))
-		total = value_one + value_two
-		data = {'total':str(total)}
-		return jsonify(data)
+		#user inputs
+		value_one = request.form.get('first')
+		value_two = request.form.get('second')
+		#api call
+		url = 'https://api.github.com/search/users?q=location:{0}+language:{1}'.format(value_one, value_two)
+		response_dict = requests.get(url).json()
+		return jsonify(response_dict)
 
 	return render_template('index.html')
 
--- app/static/main.js
+++ app/static/main.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 	$("form").on("submit", function(){
 		console.log("the form has been submitted!");
 
-		var valueOne = $('input[name="number-one"]').val()
-		var valueTwo = $('input[name="number-two"]').val()
+		var valueOne = $('input[name="location"]').val()
+		var valueTwo = $('input[name="language"]').val()
 		console.log(valueOne, valueTwo);
 
 		$.ajax({
@@ -13,9 +13,9 @@ $(document).ready(function(){
 			url: "/",
 			data: {first:valueOne, second:valueTwo},
 			success: function(results){
-				console.log(results);
+				console.log(results.items[0]);
 
-				$("#results").html(results.total)
+				$("#results").html('<a href="' + results.items[0].html_url + '" >' + results.items[0].login)
 				$("input").val("")
 			},
 			error: function(error){
